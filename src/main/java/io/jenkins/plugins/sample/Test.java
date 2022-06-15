aeb153a7c586b6abd4262919006676bc310e971a
Haibin Zang <t-haibinzang@microsoft.com>
2022-06-15 18:03:17 +0800

    initialize

---
+++ src/main/java/io/jenkins/plugins/sample/Test.java
@@ -0,0 +1,8 @@
+package io.jenkins.plugins.sample;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "\u540d\u5b57";
+        System.out.println(str);
+    }
+}
